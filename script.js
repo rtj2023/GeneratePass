53ac4db7d58541d0926df5184d4e897c27e5be33
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-01-07 19:05:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  console.log("click the button")
+
+  return "Generated password will go here!";
+
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
